6d81585bab1186aa66737b475c5a480247b92e6e
unknown <pythondeveloper441@gmail.com>
2022-06-17 14:56:29 +0500

    Fixing bugs Inshaallah

--- main_output.py
+++ main_output.py
@@ -1,10 +1,8 @@
 from fastapi.responses import JSONResponse
 from fastapi import APIRouter
 from Database import Main_output
-from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
-template = Jinja2Templates(directory="templates")
 
 
 @router.get("/search/{id}", response_class=JSONResponse)
